647f7f2cfee9ce237edf6b305f4c6fa1674f0df8
linda-05 <aa0531032025@unab.edu.sv>
2025-03-16 16:31:12 -0600

    resuelve los ejercicios

---
+++ Ejercicio_and_or_not/Ejercicios.py
@@ -0,0 +1,33 @@
+
+
+valor1 = 4 == 5 
+valor2 = 4 != 5
+valor3 = 4 <  5
+valor4 = 4 >  5
+valor5 = 4 <= 5
+valor6 = 4 >= 5
+
+# comparar elementos con los operadores boleanos
+
+v1= 2
+v2= 4
+v3= 5
+
+r1 = v1 + v2 + v3 < 25 
+print(r1)
+
+r2 = (12+3) > 78 
+print(r2)
+
+r3 = (12*8) == 12**2
+print(r3)
+
+r4 = 12* (4+5) != 12+2**2
+print(r4)
+
+r5 = 12 * (4 + 9) >= (12/2)%2 + 2 -3
+print(r5)
+
+r6 = 12 * (4+9) >= (12/2)%2 +2 -3 >10000 and 12 * (4+9) >= (12/2)%2 +2 -3 >78
+print(r6)
+
