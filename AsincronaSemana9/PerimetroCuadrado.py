8ff0590ba216569635aad3ebcafca1b9d3b064fd
linda-05 <aa0531032025@unab.edu.sv>
2025-03-19 06:51:43 -0600

    calcula el perimetro de un cuadrado

---
+++ AsincronaSemana9/PerimetroCuadrado.py
@@ -0,0 +1,9 @@
+#Perimetro del cuadrado (p) = Lado (L) * 4
+#Donde P es el perimetro y L es la longitud del lado
+
+
+def perimetroCuadrado(lado):
+    p = lado * 4
+    print ("El perimetro es", p , "unidades")
+
+perimetroCuadrado(23)
